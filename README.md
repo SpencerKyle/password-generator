7d886f913012f5a96f2bb46561a919a917e22502
Spoonska <spencerkyle@Spencers-MacBook-Pro.local>
2021-10-02 10:38:20 -0600

    added numerical, symbol, upper, lowercase logic.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # password-generator
 Challenge 03 - Password Generator
+
+Created git repo + develop branch
+
+Linked style.css and script.js to index.html
+
+added prompt boxes
--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,62 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  passLength();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
 }
 
+var passLength = function() {
+  var passQuestionLength = prompt("How many characters would you like your password to have? (You must have at least 8, with a maximum of 128.)");
+
+  if (passQuestionLength < 8) {
+    alert("Please enter a number higher than 8 and less than 128");
+    return passLength();
+  }
+  else if (passQuestionLength > 128) {
+    alert("Who would want a password this long? Please enter a number between 8 and 128.");
+    return passLength();
+  }
+  else {
+    passCharType();
+  }
+}
+
+var passCharType = function() {
+  var passLowercase = confirm("Would you like your password to contain lowercase letters?");
+  if (passLowercase) {
+    console.log('lowercase');
+  } else {
+    console.log('noLowercase');
+  }
+
+  var passUppercase = confirm("Would you like your password to contain UPPSERCASE letters?");
+  if (passUppercase) {
+    console.log('uppercase');
+  } else {
+    console.log('noUppercase');
+  }
+
+  var passNumeric = confirm("Would you like your password to numbers?");
+  if (passNumeric) {
+    console.log('numbers');
+  } else {
+    console.log('noNumbers');
+  }
+
+  var passSymbol = confirm("Would you like your password to contain symbols?");
+  if (passSymbol) {
+    console.log('symbols');
+  } else {
+    console.log('noSymbols');
+  }
+  console.log(passLowercase, passUppercase, passNumeric, passSymbol);
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
